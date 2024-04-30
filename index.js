215b00419d51b2fac6657845905ea699676db0bb
Christopher Flores <christopher321flores@gmail.com>
2024-04-29 23:49:34 -0400

    editing the none-tech-button

--- index.js
+++ index.js
@@ -9,12 +9,10 @@ function showDivs(n) {
     let x = document.getElementsByClassName("mySlides");
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length} ;
+    x[slideIndex-1].classList.remove('no-show');
     for (let i = 0; i < x.length; i++) {
-        if(i == slideIndex-1){
-            x[slideIndex-1].style.display = "block";
-        }
-        else{
-            x[i].style.display = "none";
+        if(i != slideIndex-1){
+            x[i].classList.add('no-show');
         }
     }
 }
