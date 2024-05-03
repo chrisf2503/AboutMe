4c777a75e376c67d490a15207bd1c622b2664834
Christopher Flores <christopher321flores@gmail.com>
2024-05-03 09:36:32 -0400

    adding animation

--- index.js
+++ index.js
@@ -16,3 +16,30 @@ function showDivs(n) {
         }
     }
 }
+let animation = {
+    revealDistance: 150,
+    initialOpacity: 0,
+    transitionDelay: 0, 
+    transitionDuration: '2s',
+    transitionProperty: 'all',
+    trnasitionTimingFunction: 'ease'
+}  
+
+let revealableContainers = document.getElementsByClassName('revealable')
+
+const reveal = () => {
+    console.log(revealableContainers.length);
+    for(let i = 0; i < revealableContainers.length; i++){
+        let windowHeight = window.innerHeight;
+        let topOfRevealableContainer = revealableContainers[i].getBoundingClientRect().top;
+        if (topOfRevealableContainer < windowHeight - animation.revealDistance) {
+            /* add the active class to the revealableContainer's classlist */
+            revealableContainers[i].classList.add('action');
+        }else {
+            /* remove the active class to the revealableContainer's classlist */
+            revealableContainers[i].classList.remove('action');
+        }
+
+    }
+}
+window.addEventListener("scroll",reveal)
